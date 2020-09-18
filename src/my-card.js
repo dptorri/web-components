d4078251ea5bf9786a9ffa20c6db413da6c3f78d
Daniel Torri <dptorri@gmail.com>
2020-09-18 17:19:53 +0200

    Add prettier and styled shadow DOM

--- src/my-card.js
+++ src/my-card.js
@@ -1,7 +1,7 @@
 class MyCard extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = 'My Card';
-    }
+  connectedCallback() {
+    this.innerHTML = "My Card";
+  }
 }
 
-window.customElements.define('my-card', MyCard);
+window.customElements.define("my-card", MyCard);
